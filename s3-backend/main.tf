6a0c2c664c757655cede3357b6d6df82b767c5ca
Jian Han <jianhan@mail.com>
2020-04-03 21:25:48 +1000

    Added lambda data s3 bucket.

--- s3-backend/main.tf
+++ s3-backend/main.tf
@@ -61,6 +61,28 @@ resource "aws_s3_bucket" "lambdas" {
 }
 
 # ------------------------------------------------------------------------------
+# CREATE THE S3 BUCKET to store all data lambdas produce.
+# ------------------------------------------------------------------------------
+resource "aws_s3_bucket" "lambda_data" {
+  bucket = "jian-personal-lambda-data"
+
+  # Enable versioning so we can see the full revision history of our
+  # state files
+  versioning {
+    enabled = true
+  }
+
+  # Enable server-side encryption by default
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+# ------------------------------------------------------------------------------
 # CREATE THE DYNAMODB TABLE
 # ------------------------------------------------------------------------------
 resource "aws_dynamodb_table" "terraform_locks" {
